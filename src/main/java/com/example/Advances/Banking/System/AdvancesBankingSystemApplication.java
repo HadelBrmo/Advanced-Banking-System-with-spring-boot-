7e61660a8a17327f78dc1514ca941e941f937b3e
Hadel Brmo <brmohadel56@gmail.com>
2025-12-07 14:24:27 +0300

    chore : initialize banking system project and core dependencies

---
+++ src/main/java/com/example/Advances/Banking/System/AdvancesBankingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.Advances.Banking.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancesBankingSystemApplication {
+
+	public static void main(String[] args) {
+//
+        SpringApplication.run(AdvancesBankingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Advances/Banking/System/AdvancesBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Advances.Banking.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancesBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
